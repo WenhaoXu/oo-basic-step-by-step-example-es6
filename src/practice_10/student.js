128a7bcf351abb52578769fde5d15ae3c515b166
XUHO3 <howells.xu@oocl.com>
2018-07-09 19:51:18 +0800

    update unit_practice10 and complete it

--- src/practice_10/student.js
+++ src/practice_10/student.js
@@ -1,1 +1,24 @@
 
+
+const {Person}= require("./person");
+class Student  extends Person{
+    constructor(id,name ,age,Class){
+        super(id,name,age);
+        this.klass=Class;
+    }
+    introduce(){
+        if(this.klass.leader===this){
+            return  super.introduce()+" I am a Student. I am Leader of "+this.klass.getDisplayName()+".";
+        }
+        return super.introduce()+" I am a Student. I am at "+this.klass.getDisplayName()+".";
+    }
+    isInClass(teacher){
+     return     this.klass.isIn(teacher.klasses);
+    }
+}
+
+
+
+
+
+module.exports={Student}
--- src/practice_10/teacher.js
+++ src/practice_10/teacher.js
@@ -1,1 +1,29 @@
 
+const {Person}= require("./person");
+class Teacher  extends Person{
+    constructor(id,name ,age,klasses){
+        super(id,name,age);
+        this.klasses=klasses ||[];
+    }
+    introduce(){
+        if((this.klasses.length === 0))
+            return super.introduce()+" I am a Teacher. I teach No Class.";
+        else {
+            return super.introduce() + " I am a Teacher. I teach Class " + this.klasses.map(klass=>{return klass.number;}).join(", ") + ".";
+        }
+    }
+    introduceWith(student){
+        if(this.klasses.includes(student.klass)){
+            return super.introduce() + " I am a Teacher. I teach "+student.name+".";
+        }
+        else{
+            return super.introduce() + " I am a Teacher. I don't teach "+student.name+".";
+        }
+    }
+    isTeaching(student){
+        return   student.isInClass(teacher)
+    }
+}
+
+
+module.exports={Teacher}
